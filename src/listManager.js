6cf26d21a96f430108b8055300819d357300759e
DrantDumani <darnjames@gmail.com>
2023-04-14 19:49:08 -0400

    removed ability to change value of list in listManager

--- src/listManager.js
+++ src/listManager.js
@@ -1,10 +1,19 @@
-function listManager(){
-    let list = []
-    const addItem = item => {list.push(item)}
-    const editItem = (index, item) => {list[index] = item}
-    const deleteItem = item => {list = list.filter(el => el !== item)}
-    const getList = () => list
-    return {addItem, editItem, deleteItem, getList}
+function listManager() {
+  const list = [];
+  const addItem = (item) => {
+    list.push(item);
+  };
+  const editItem = (index, item) => {
+    list[index] = item;
+  };
+  const deleteItem = (item) => {
+    list.splice(list.indexOf(item), 1);
+  };
+  const getList = () => list;
+  const setList = (arr) => {
+    list = arr;
+  };
+  return { addItem, editItem, deleteItem, getList, setList };
 }
 
-export default listManager
+export default listManager;
