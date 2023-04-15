fb856f0c4df55bbb5efeb49323db5e346ae54564
DrantDumani <darnjames@gmail.com>
2023-04-15 03:00:02 -0400

    Added callback function to fetchUserTasks in firestore.js

--- src/firestore.js
+++ src/firestore.js
@@ -14,6 +14,7 @@ export async function fetchUserTasks(userId, callback) {
   const docRef = doc(database, "userTasks", userId);
   const docSnap = await getDoc(docRef);
   if (docSnap.exists()) {
+    callback(docSnap.data());
   } else {
     setDoc(doc(database, "userTasks", userId), {
       projects: [],
